4f3c1832edba5473b44499393e2378e8d16578a9
LuizantonioDev9 <luizantoniodev9@gmail.com>
2025-01-14 14:56:07 -0300

    criando a entidade seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
+        Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),3000.0,obj);
         System.out.println(obj);
+        System.out.println(seller);
+
+
     }
 }
